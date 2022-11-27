69be830f6fd27d7f4d88ff543505b96c73b62d8c
winstonlopez <ev.winston2304@gmail.com>
2022-11-27 20:14:47 +0800

    fixing the drop down menu

--- main.js
+++ main.js
@@ -1,2 +1,9 @@
 'use strict'
 
+
+centerContainer.addEventListener('click', function(event){
+    if(!event.target.closest('span.menu, span.menu account'))return;    //if not on menu, do nothing
+
+    let elem = event.target.closest(`span.menu, span.menu account`).querySelector('span.drop-down');
+        elem.hidden = !elem.hidden;
+})
